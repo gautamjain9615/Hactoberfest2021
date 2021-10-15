9236d2b1bc9057cb74d3fdffa59314de55a65276
sushilmahtha <57331986+sushilmahtha@users.noreply.github.com>
2021-10-15 23:54:24 +0530

    Count triplets with sum smaller than X
    Given an array arr[] of distinct integers of size N and a value sum, the task is to find the count of triplets (i, j, k), having (i<j<k) with the sum of (arr[i] + arr[j] + arr[k]) smaller than the given value sum.

---
+++ Count triplets with sum smaller than X .cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std; 
+class Solution{	
+	public:
+	long long countTriplets(long long arr[], int n, long long sum)
+	{
+	    sort(arr,arr+n);
+	   long long int ans = 0;
+
+      for (int i = 0; i < n - 2; i++)
+      {
+          int j = i + 1, k = n - 1;
+          while (j < k)
+          {
+              if (arr[i] + arr[j] + arr[k] >= sum)
+              k--;
+              else
+              {
+                  ans += (k - j);
+                  j++;
+              }
+          }
+      }
+      return ans;    
+	}
+		 
+};
+int main() 
+{   	
+    	int n;
+    	long long sum;
+        cin>>n>>sum;
+        long long arr[n];
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout << ob.countTriplets(arr, n, sum) ;        
+	    cout << "\n"; 
+    return 0;
+}
+
+  
