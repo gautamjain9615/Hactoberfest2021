4bdd8e61d0efc14a98fe61d38f01b31cae06f96f
Qaisarali Sulaimani <72247744+Qaisarali-Sulaimani@users.noreply.github.com>
2021-10-22 22:52:12 +0530

    Added a program to check a string is present as substring or not

---
+++ stringAsSubstring.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool find_string(string& x, string& y)
+{
+    if(x.find(y) != string::npos)
+        return 1;
+
+    return 0;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+
+    return 0;
+}
